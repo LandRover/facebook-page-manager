ea30fa50d60d5153a874300c6f2797be6d0565ac
Oleg <github.com@landroverz.com>
2015-07-18 10:23:21 -0400

    dbinsert replaced with Seeders. More of a Laravel way.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,14 +13,3 @@ Route::get('/', [
 Route::get('/login', 'LoginController@fbLogin'); // regular login, creates user if doesnt exist.
 Route::get('/join/{userHash}', 'LoginController@fbLogin'); // agent join, hash is required.
 
-Route::group(['prefix' => 'dbimport'], function() {
-    Route::get('/', [
-        'as' => 'GOTO_DB_IMPORT',
-        'uses' => 'DBImportController@createSchema'
-    ]);
-    
-    Route::get('insert', [
-        'as' => 'GOTO_DB_INSERT',
-        'uses' => 'DBImportController@insertInitialData'
-    ]);
-});
