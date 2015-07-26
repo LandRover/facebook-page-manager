b1cabb075e96d2c2366329065c48429c88f0e2ca
Oleg <github.com@landroverz.com>
2015-07-26 04:19:47 -0400

    proper files copy to public/

--- build/tasks/copy.js
+++ build/tasks/copy.js
@@ -3,21 +3,21 @@ module.exports = function(grunt, options) {
     return {
         fonts: {
             files: [
-                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/font-awesome/fonts', dest: '<%= build.paths.debug.fonts %>'},
-                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/material-design-iconic-font/dist/fonts', dest: '<%= build.paths.debug.fonts %>'},
-                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/roboto-fontface/fonts', dest: '<%= build.paths.debug.fonts %>'},
-                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/weather-icons/font', dest: '<%= build.paths.debug.fonts %>'},
-                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/bootstrap-sass/assets/fonts/bootstrap', dest: '<%= build.paths.debug.fonts %>'}
+                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/font-awesome/fonts', dest: '<%= build.paths.build.fonts %>'},
+                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/material-design-iconic-font/dist/fonts', dest: '<%= build.paths.build.fonts %>'},
+                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/roboto-fontface/fonts', dest: '<%= build.paths.build.fonts %>'},
+                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/weather-icons/font', dest: '<%= build.paths.build.fonts %>'},
+                {expand: true, src: '**', cwd: '<%= build.paths.lib.node %>/bootstrap-sass/assets/fonts/bootstrap', dest: '<%= build.paths.build.fonts %>'}
             ]
         },
         
         
-        debug: {
+        staticAssets: {
             files: [
-                {expand: true, src: '**/*', cwd: '<%= build.paths.src.dir %>/html', dest: '<%= build.paths.debug.assets %>/html'},
-                {expand: true, src: '**/*', cwd: '<%= build.paths.src.dir %>/images', dest: '<%= build.paths.debug.assets %>/images'},
-                {src: '<%= build.paths.lib.bower %>/ng-file-upload/FileAPI.flash.swf', dest: '<%= build.paths.debug.assets %>/FileAPI.flash.swf'},
-                {src: '<%= build.paths.lib.bower %>/ng-file-upload/FileAPI.min.js', dest: '<%= build.paths.debug.assets %>/FileAPI.min.js'}
+                {expand: true, src: '**/*', cwd: '<%= build.paths.src.dir %>/html', dest: '<%= build.paths.build.assets %>/html'},
+                {expand: true, src: '**/*', cwd: '<%= build.paths.src.dir %>/images', dest: '<%= build.paths.build.assets %>/images'},
+                {src: '<%= build.paths.lib.bower %>/ng-file-upload/FileAPI.flash.swf', dest: '<%= build.paths.build.assets %>/FileAPI.flash.swf'},
+                {src: '<%= build.paths.lib.bower %>/ng-file-upload/FileAPI.min.js', dest: '<%= build.paths.build.assets %>/FileAPI.min.js'}
             ]
         }
     };
